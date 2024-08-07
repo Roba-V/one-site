f27f05143339786e37098d5a9c8b57af98e6e2f1
Roba-V <roba@vrstu.net>
2024-08-08 01:59:49 +0900

    chore: #12 Install Chakra UI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import {Button} from "@chakra-ui/react";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -18,9 +18,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+          <Button colorScheme="blue" onClick={() => setCount((count) => count + 1)}>count is {count}</Button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
