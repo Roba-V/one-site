5edaf5a9dfb8d3e2cecea4cb296fab8ea4356b35
Roba-V <roba@vrstu.net>
2024-08-09 21:17:51 +0900

    feat: #12 Create Chakra sample

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,31 +1,36 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import {Button} from "@chakra-ui/react";
+import {Box, Button, Center, Grid, GridItem} from "@chakra-ui/react";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-          <Button colorScheme="blue" onClick={() => setCount((count) => count + 1)}>count is {count}</Button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <Box p={8} maxW="960px" mx="auto">
+            <Grid templateColumns="repeat(2, minmax(300px, 1fr))}">
+                <GridItem w="100%" h="100">
+                    <Center h="100%">
+                        <a href="https://vitejs.dev" target="_blank">
+                            <img src={viteLogo} className="logo" alt="Vite logo"/>
+                        </a>
+                    </Center>
+                </GridItem>
+                <GridItem w="100%" h="100">
+                    <Center h="100%">
+                        <a href="https://react.dev" target="_blank">
+                            <img src={reactLogo} className="logo react" alt="React logo"/>
+                        </a>
+                    </Center>
+                </GridItem>
+            </Grid>
+        </Box>
+        <Box p={8} maxW="960px" mx="auto">
+            <Center>
+                <Button colorScheme="blue" onClick={() => setCount((count) => count + 1)}>count is {count}</Button>
+            </Center>
+        </Box>
     </>
   )
 }
